885cd2e57ddcc29cae0356a278f8aee901161086
Pavan543210 <135861719+Pavan543210@users.noreply.github.com>
2023-09-29 14:12:55 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,144 @@
+const wrapper = document.querySelector(".sliderWrapper");
+const menuItems = document.querySelectorAll(".menuItem");
+const currencySymbol = "₹"; // Indian Rupee symbol
+
+const products = [
+  {
+    id: 1,
+    title: "Air Force",
+    price: 9880, // Set the price in Indian Rupees
+    colors: [
+      {
+        code: "black",
+        img: "./img/air.png",
+      },
+      {
+        code: "darkblue",
+        img: "./img/air2.png",
+      },
+    ],
+  },
+  {
+    id: 2,
+    title: "Air Jordan",
+    price: 12300,
+    colors: [
+      {
+        code: "lightgray",
+        img: "./img/jordan.png",
+      },
+      {
+        code: "green",
+        img: "./img/jordan2.png",
+      },
+    ],
+  },
+  {
+    id: 3,
+    title: "Blazer",
+    price: 9050,
+    colors: [
+      {
+        code: "lightgray",
+        img: "./img/blazer.png",
+      },
+      {
+        code: "green",
+        img: "./img/blazer2.png",
+      },
+    ],
+  },
+  {
+    id: 4,
+    title: "Crater",
+    price: 10700,
+    colors: [
+      {
+        code: "black",
+        img: "./img/crater.png",
+      },
+      {
+        code: "lightgray",
+        img: "./img/crater2.png",
+      },
+    ],
+  },
+  {
+    id: 5,
+    title: "Hippie",
+    price: 8220,
+    colors: [
+      {
+        code: "gray",
+        img: "./img/hippie.png",
+      },
+      {
+        code: "black",
+        img: "./img/hippie2.png",
+      },
+    ],
+  },
+  // Add other products here with their prices in Indian Rupees
+];
+
+let chosenProduct = products[0];
+
+const currentProductImg = document.querySelector(".productImg");
+const currentProductTitle = document.querySelector(".productTitle");
+const currentProductPrice = document.querySelector(".productPrice");
+const currentProductColors = document.querySelectorAll(".color");
+const currentProductSizes = document.querySelectorAll(".size");
+
+// Function to update displayed currency
+function updateCurrency(price) {
+  return currencySymbol + price.toLocaleString("en-IN");
+}
+
+menuItems.forEach((item, index) => {
+  item.addEventListener("click", () => {
+    // Change the current slide
+    wrapper.style.transform = `translateX(${-100 * index}vw)`;
+
+    // Change the chosen product
+    chosenProduct = products[index];
+
+    // Change texts of currentProduct
+    currentProductTitle.textContent = chosenProduct.title;
+    currentProductPrice.textContent = updateCurrency(chosenProduct.price);
+    currentProductImg.src = chosenProduct.colors[0].img;
+
+    // Assign new colors
+    currentProductColors.forEach((color, index) => {
+      color.style.backgroundColor = chosenProduct.colors[index].code;
+    });
+  });
+});
+
+currentProductColors.forEach((color, index) => {
+  color.addEventListener("click", () => {
+    currentProductImg.src = chosenProduct.colors[index].img;
+  });
+});
+
+currentProductSizes.forEach((size, index) => {
+  size.addEventListener("click", () => {
+    currentProductSizes.forEach((size) => {
+      size.style.backgroundColor = "white";
+      size.style.color = "black";
+    });
+    size.style.backgroundColor = "black";
+    size.style.color = "white";
+  });
+});
+
+const productButton = document.querySelector(".productButton");
+const payment = document.querySelector(".payment");
+const close = document.querySelector(".close");
+
+productButton.addEventListener("click", () => {
+  payment.style.display = "flex";
+});
+
+close.addEventListener("click", () => {
+  payment.style.display = "none";
+});
